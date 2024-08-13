14138d23027dbd87465c2b87632d1c8b392c7cdf
tube0330 <zien0516@naver.com>
2024-08-13 09:33:34 +0900

    240813

--- Assets/02.Scripts/FollowCam.cs
+++ Assets/02.Scripts/FollowCam.cs
@@ -6,19 +6,23 @@ public class FollowCam : MonoBehaviour
 {
     [SerializeField] Transform camTr;
     [SerializeField] Transform targetTr;    //카메라가 따라다닐 대상
-    [SerializeField] float h;
-    [SerializeField] float dis;
+    [SerializeField] float h = 3f;
+    [SerializeField] float dis = 5f;
     [SerializeField] float targetOffset = 1.0f;
     [SerializeField] float moveDamping = 5.0f;  //급격하게 움직일 때 떨림 방지 위해 선언
     [SerializeField] float rotDamping = 10f;
 
     void Start()
     {
-        
+        camTr = transform;
     }
 
     void LateUpdate()
     {
-        
+        var camPos = targetTr.position - (Vector3.forward * dis) + (Vector3.up * h);
+        camTr.position = Vector3.Slerp(camTr.position, camPos, Time.deltaTime * moveDamping);
+        camTr.rotation = Quaternion.Slerp(camTr.rotation, targetTr.rotation, Time.deltaTime * rotDamping);
+
+        camTr.LookAt(targetTr.position + Vector3.up * targetOffset);
     }
 }
